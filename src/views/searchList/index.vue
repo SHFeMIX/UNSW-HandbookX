e2a4fd5c5264741e5e588e0f3cec9a55fb1780f9
邵竞帆 <alan@shaojingfandeMac-mini.local>
2025-03-02 23:19:09 +0800

    header in serachList page

--- src/views/searchList/index.vue
+++ src/views/searchList/index.vue
@@ -1,8 +1,21 @@
 <template>
-  {{ route.query }}
+  <div class="searchList">
+    <Header />
+    {{ route.query }}
+</div>
 </template>
 
 <script setup lang="ts">
+import Header from '@/common/components/Header.vue';
 import { useRoute } from 'vue-router'
 const route = useRoute()
 </script>
+
+<style scoped>
+.searchList {
+  width: 1440px;
+  height: 840px;
+
+  margin: 50px auto;
+}
+</style>
