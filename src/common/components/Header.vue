d7488bf8c51c86cd28c90ac120a26091664e2511
邵竞帆 <alan@shaojingfandeMac-mini.local>
2025-07-17 23:33:49 +0800

    unocss 属性化模式

--- src/common/components/Header.vue
+++ src/common/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div bg="#030303" w-1440px h-72px flex items-center text-white font-normal text-base tracking-0.1px leading-22.4px
+  <div bg="#030303" w-1440px h-72px flex items-center text-white font-normal text-base tracking-0.1px leading-22.4px font-Kokoro
     style="font-family: Kokoro">
     <img :src="P" class="self-start w-[48px] h-[54.34px] mt-[5px] ml-[54px] mr-[18px]" />
     UNSW Handbook Planner
